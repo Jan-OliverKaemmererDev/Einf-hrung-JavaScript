45187fb06f64da8399ea0e0fff85e0ecb064374e
Jan-Oliver <jowieja22@gmail.com>
2025-10-21 13:05:02 +0200

    added objects lesson

---
+++ objects.js
@@ -0,0 +1,8 @@
+let myFancyObject = {
+    age : 32,
+    height : 173,
+    name : "Asterix",
+}
+
+console.log(myFancyObject.age);
+// Man kann den KonsolenLog auch so schreiben: console.log(myFancyObject["age"]);
