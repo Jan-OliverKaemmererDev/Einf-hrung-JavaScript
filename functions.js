d3fa301629b126afdaa7022745b0dd85c9919ec6
Jan-Oliver <jowieja22@gmail.com>
2025-10-21 11:54:42 +0200

    added parms in functions lesson

--- functions.js
+++ functions.js
@@ -1,12 +1,26 @@
+// Funktionen Grundwissen
+// Der Code wird zwar von oben nach unten gelesen, jedoch kann eine function auch ganz unten stehen.
+
 let discount = 50;
 let price = 500;
 
-console.log((price - discount) * 1.19)
+calculatePrice()
 
 price = 400;
 
-
+calculatePrice()
 
 function calculatePrice() {
-    
+    console.log((price - discount) * 1.19);
+}
+
+
+// Grundwissen zu parms (Parametern)
+// Ein Parameter ist ein Platzhalter.
+
+logCalculatedPrice(50, 500);
+logCalculatedPrice(50, 300);
+
+function logCalculatedPrice(discount, price) {
+    console.log((price - discount) * 1.19);
 }
